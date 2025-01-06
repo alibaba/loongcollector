9555e088501595655b45c65d4ef34c58c7e6140f
liqiang <liqiang.x@foxmail.com>
2025-01-06 03:01:45 +0000

    feat: support external_labels

--- core/prometheus/Constants.h
+++ core/prometheus/Constants.h
@@ -79,6 +79,7 @@ const char* const KEY_FILE = "key_file";
 const char* const SERVER_NAME = "server_name";
 const char* const HOST = "Host";
 const char* const INSECURE_SKIP_VERIFY = "insecure_skip_verify";
+const char* const EXTERNAL_LABELS = "external_labels";
 
 // scrape protocols, from https://prometheus.io/docs/prometheus/latest/configuration/configuration/#scrape_config
 // text/plain, application/openmetrics-text will be used
