356dcbf7488a47de60c203b87f1e48cf0884023d
panjxhhhhh <panjinxing.pjx@alibaba-inc.com>
2025-03-21 14:38:53 +0800

    remove unused test
    Change-Id: I9234b65194bae32231b5a6e4cd317c554bbd9e20

--- plugins/flusher/odps/flusher_odps_test.go
+++ plugins/flusher/odps/flusher_odps_test.go
@@ -15,66 +15,12 @@
 package odps
 
 import (
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/alibaba/ilogtail/pkg/protocol"
-	"github.com/alibaba/ilogtail/plugins/test"
-	"github.com/alibaba/ilogtail/plugins/test/mock"
 )
 
 func TestFlusherOdps(t *testing.T) {
 	d := NewFlusherOdps()
 	require.NotNil(t, d)
-}
-
-func TestConnectAndWrite(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping integratio n test in short mode")
-	}
-
-	projectName := "test_logagent_to_mc"
-	tableName := "blob_1"
-	partitionConfig := "pt=%Y%m%d"
-	d := &FlusherOdps{
-		AccessKeyID:     "xx",
-		AccessKeySecret: "xx",
-		Endpoint:        "",
-		ProjectName:     projectName,
-		TableName:       tableName,
-		TimeRange:       60,
-		PartitionConfig: partitionConfig,
-	}
-
-	lctx := mock.NewEmptyContext("p", "l", "c")
-	err := d.Init(lctx)
-	require.NoError(t, err)
-
-	lgl := makeTestLogGroupList()
-
-	err = d.Flush(projectName, tableName, "configName", lgl.GetLogGroupList())
-	require.NoError(t, err)
-	_ = d.Stop()
-}
-
-func makeTestLogGroupList() *protocol.LogGroupList {
-	f := map[string]string{}
-	lgl := &protocol.LogGroupList{
-		LogGroupList: make([]*protocol.LogGroup, 0, 10),
-	}
-	for i := 1; i <= 10; i++ {
-		lg := &protocol.LogGroup{
-			Logs: make([]*protocol.Log, 0, 10),
-		}
-		for j := 1; j <= 10; j++ {
-			f["field1"] = strconv.Itoa(i)
-			f["field2"] = "The message: " + strconv.Itoa(j)
-			l := test.CreateLogByFields(f)
-			lg.Logs = append(lg.Logs, l)
-		}
-		lgl.LogGroupList = append(lgl.LogGroupList, lg)
-	}
-	return lgl
 }
