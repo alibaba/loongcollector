64a9c88f07753b27da9cfd1d11ec4d16966fb95c
wanruiping.wrp <wanruiping.wrp@alibaba-inc.com>
2025-05-14 14:44:44 +0800

    fix: add metric calculate ut and del a macro definition

--- core/host_monitor/common/FieldEntry.h
+++ core/host_monitor/common/FieldEntry.h
@@ -1,11 +1,11 @@
-/**
- * Copyright 2025 Alibaba Cloud
+/*
+ * Copyright 2025 iLogtail Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-#ifndef LOONGCOLLECTOR_FIELD_ENTRY_H
-#define LOONGCOLLECTOR_FIELD_ENTRY_H
 #include <functional>
 #include <string>
 
@@ -37,5 +35,3 @@ public:
 
     TFieldType& value(TClass& p) const { return const_cast<TFieldType&>(_fnGet(p)); }
 };
-
-#endif // LOONGCOLLECTOR_FIELD_ENTRY_H
