94d35ab4a106c2edd136f5708e0ab857543760d6
bernie <zzxddyx@163.com>
2025-05-14 17:30:08 +0800

    processor_add_fields_v2

--- plugins/processor/addfieldsv2/processor_add_fields_v2_test.go
+++ plugins/processor/addfieldsv2/processor_add_fields_v2_test.go
@@ -4,10 +4,11 @@ import (
 	"os"
 	"testing"
 
-	"github.com/alibaba/ilogtail/pkg/protocol"
-	"github.com/alibaba/ilogtail/plugins/test/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/alibaba/ilogtail/pkg/protocol"
+	"github.com/alibaba/ilogtail/plugins/test/mock"
 )
 
 func newProcessor() (*ProcessorAddFields, error) {
