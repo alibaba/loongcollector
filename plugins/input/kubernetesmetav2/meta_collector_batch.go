a6b943ccd65995f8c2f8bbbdefd529459522127a
阿柄 <bingchang.cbc@alibaba-inc.com>
2025-05-15 10:18:56 +0800

    fix

--- plugins/input/kubernetesmetav2/meta_collector_batch.go
+++ plugins/input/kubernetesmetav2/meta_collector_batch.go
@@ -1,7 +1,6 @@
 package kubernetesmetav2
 
 import (
-	"strconv"
 	"time"
 
 	batch "k8s.io/api/batch/v1" //nolint:typecheck
@@ -32,21 +31,9 @@ func (m *metaCollector) processJobEntity(data *k8smeta.ObjectWrapper, method str
 			containerInfos = append(containerInfos, containerInfo)
 		}
 		log.Contents.Add("containers", m.processEntityJSONArray(containerInfos))
-		if obj.Spec.Suspend != nil {
-			log.Contents.Add("suspend", strconv.FormatBool(*obj.Spec.Suspend))
-		} else {
-			log.Contents.Add("suspend", "")
-		}
-		if obj.Spec.BackoffLimit != nil {
-			log.Contents.Add("backoff_limit", strconv.FormatInt(int64(*obj.Spec.BackoffLimit), 10))
-		} else {
-			log.Contents.Add("backoff_limit", "")
-		}
-		if obj.Spec.Completions != nil {
-			log.Contents.Add("completion", strconv.FormatInt(int64(*obj.Spec.Completions), 10))
-		} else {
-			log.Contents.Add("completion", "")
-		}
+		log.Contents.Add("suspend", safeGetBoolString(obj.Spec.Suspend))
+		log.Contents.Add("backoff_limit", safeGetInt32String(obj.Spec.BackoffLimit))
+		log.Contents.Add("completion", safeGetInt32String(obj.Spec.Completions))
 		return []models.PipelineEvent{log}
 	}
 	return nil
@@ -65,11 +52,7 @@ func (m *metaCollector) processCronJobEntity(data *k8smeta.ObjectWrapper, method
 		log.Contents.Add("labels", m.processEntityJSONObject(obj.Labels))
 		log.Contents.Add("annotations", m.processEntityJSONObject(obj.Annotations))
 		log.Contents.Add("schedule", obj.Spec.Schedule)
-		if obj.Spec.Suspend != nil {
-			log.Contents.Add("suspend", strconv.FormatBool(*obj.Spec.Suspend))
-		} else {
-			log.Contents.Add("suspend", "")
-		}
+		log.Contents.Add("suspend", safeGetBoolString(obj.Spec.Suspend))
 		return []models.PipelineEvent{log}
 	}
 	return nil
