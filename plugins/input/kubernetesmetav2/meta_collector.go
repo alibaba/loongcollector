1f80cab3ecfe5d6f949d906055eeb126f3f65c07
阿柄 <bingchang.cbc@alibaba-inc.com>
2025-05-29 13:48:14 +0800

    fix

--- plugins/input/kubernetesmetav2/meta_collector.go
+++ plugins/input/kubernetesmetav2/meta_collector.go
@@ -21,6 +21,8 @@ import (
 	"github.com/alibaba/ilogtail/pkg/protocol"
 )
 
+const emptyJsonObject = "{}"
+
 type metaCollector struct {
 	serviceK8sMeta *ServiceK8sMeta
 	collector      pipeline.Collector
@@ -302,12 +304,12 @@ func (m *metaCollector) processEntityLinkCommonPart(logContents models.LogConten
 
 func (m *metaCollector) processEntityJSONObject(obj interface{}) string {
 	if obj == nil {
-		return "{}"
+		return emptyJsonObject
 	}
 	objStr, err := json.Marshal(obj)
 	if err != nil {
 		logger.Error(context.Background(), "PROCESS_ENTITY_JSON_OBJECT_FAIL", "process entity json object fail", err)
-		return "{}"
+		return emptyJsonObject
 	}
 	return string(objStr)
 }
