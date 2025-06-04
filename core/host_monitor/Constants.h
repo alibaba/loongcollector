2d19a1dc6f580030965432c16ba386b2db3693b1
wanruiping.wrp <wanruiping.wrp@alibaba-inc.com>
2025-06-04 17:29:06 +0800

    feature: add net collector

--- core/host_monitor/Constants.h
+++ core/host_monitor/Constants.h
@@ -26,7 +26,13 @@ extern std::filesystem::path PROCESS_DIR;
 const extern std::filesystem::path PROCESS_STAT;
 const extern std::filesystem::path PROCESS_LOADAVG;
 const extern std::filesystem::path PROCESS_MEMINFO;
+
+const extern std::filesystem::path PROCESS_NET_SOCKSTAT;
+const extern std::filesystem::path PROCESS_NET_SOCKSTAT6;
+const extern std::filesystem::path PROCESS_NET_DEV;
+const extern std::filesystem::path PROCESS_NET_IF_INET6;
 const extern int64_t SYSTEM_HERTZ;
+const extern int NET_INTERFACE_LIST_MAX;
 
 #ifdef __ENTERPRISE__
 inline constexpr StringView DEFAULT_INSTANCE_ID_LABEL = "instance_id";
--- core/host_monitor/LinuxSystemInterface.h
+++ core/host_monitor/LinuxSystemInterface.h
@@ -40,6 +40,9 @@ private:
     bool GetProcessListInformationOnce(ProcessListInformation& processListInfo) override;
     bool GetProcessInformationOnce(pid_t pid, ProcessInformation& processInfo) override;
     bool GetHostMemInformationStatOnce(MemoryInformation& meminfoStr) override;
+    bool GetTCPStatInformationOnce(TCPStatInformation& tcpStatInfo) override;
+    bool GetNetRateInformationOnce(NetRateInformation& netRateInfo) override;
+    bool GetNetInterfaceInformationOnce(NetInterfaceInformation& netInterfaceInfo) override;
 
     bool GetSystemLoadInformationOnce(SystemLoadInformation& systemLoadInfo) override;
     bool GetCPUCoreNumInformationOnce(CpuCoreNumInformation& cpuCoreNumInfo) override;
