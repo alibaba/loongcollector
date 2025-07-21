7ec06a3f7a5832f1355b8b3100fc465542ba62d7
StartE <yili0618@qq.com>
2025-07-21 11:39:05 +0000

    revert metric changes

--- pkg/helper/k8smeta/k8s_meta_manager.go
+++ pkg/helper/k8smeta/k8s_meta_manager.go
@@ -62,23 +62,6 @@ type MetaManager struct {
 	httpRequestCount   selfmonitor.CounterMetric
 	httpAvgDelayMs     selfmonitor.CounterMetric
 	httpMaxDelayMs     selfmonitor.GaugeMetric
-
-	//details metrics
-	podSizeGauge              selfmonitor.GaugeMetric
-	nodeSizeGauge             selfmonitor.GaugeMetric
-	serviceSizeGauge          selfmonitor.GaugeMetric
-	deploymentSizeGauge       selfmonitor.GaugeMetric
-	daemonsetSizeGauge        selfmonitor.GaugeMetric
-	statefulSetSizeGauge      selfmonitor.GaugeMetric
-	configMapSizeGauge        selfmonitor.GaugeMetric
-	jobSizeGauge              selfmonitor.GaugeMetric
-	cronJobSizeGauge          selfmonitor.GaugeMetric
-	namespaceSizeGauge        selfmonitor.GaugeMetric
-	persistentVolumeSizeGauge selfmonitor.GaugeMetric
-	pvcSizeGauge              selfmonitor.GaugeMetric
-	storageClassSizeGauge     selfmonitor.GaugeMetric
-	ingressSizeGauge          selfmonitor.GaugeMetric
-	replicasetSizeGauge       selfmonitor.GaugeMetric
 }
 
 func GetMetaManagerInstance() *MetaManager {
@@ -125,23 +108,6 @@ func (m *MetaManager) Init(configPath string) (err error) {
 	m.httpRequestCount = selfmonitor.NewCounterMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaHTTPRequestTotal)
 	m.httpAvgDelayMs = selfmonitor.NewAverageMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaHTTPAvgDelayMs)
 	m.httpMaxDelayMs = selfmonitor.NewMaxMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaHTTPMaxDelayMs)
-
-	//add k8s detail metrics
-	m.podSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaPodSize)
-	m.nodeSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaNodeSize)
-	m.serviceSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaServiceSize)
-	m.deploymentSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaDeploymentSize)
-	m.daemonsetSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaDaemonsetSize)
-	m.statefulSetSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaStatefulSetSize)
-	m.configMapSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaConfigMapSize)
-	m.jobSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaJobSize)
-	m.cronJobSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaCronJobSize)
-	m.namespaceSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaNamspaceSize)
-	m.pvcSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaPVCSize)
-	m.persistentVolumeSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaPersistentVolumeSize)
-	m.storageClassSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaStorageClassSize)
-	m.ingressSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaIngressSize)
-	m.replicasetSizeGauge = selfmonitor.NewGaugeMetricAndRegister(&m.metricRecord, selfmonitor.MetricRunnerK8sMetaReplicaSetSize)
 
 	go func() {
 		startTime := time.Now()
@@ -231,42 +197,6 @@ func GetMetaManagerMetrics() []map[string]string {
 	}
 	manager.queueSizeGauge.Set(float64(queueSize))
 	manager.cacheResourceGauge.Set(float64(cacheSize))
-
-	for name, cache := range manager.cacheMap {
-		switch name {
-		case POD:
-			manager.podSizeGauge.Set(float64(cache.GetSize()))
-		case NODE:
-			manager.nodeSizeGauge.Set(float64(cache.GetSize()))
-		case SERVICE:
-			manager.serviceSizeGauge.Set(float64(cache.GetSize()))
-		case DEPLOYMENT:
-			manager.deploymentSizeGauge.Set(float64(cache.GetSize()))
-		case DAEMONSET:
-			manager.daemonsetSizeGauge.Set(float64(cache.GetSize()))
-		case STATEFULSET:
-			manager.statefulSetSizeGauge.Set(float64(cache.GetSize()))
-		case CONFIGMAP:
-			manager.configMapSizeGauge.Set(float64(cache.GetSize()))
-		case JOB:
-			manager.jobSizeGauge.Set(float64(cache.GetSize()))
-		case CRONJOB:
-			manager.cronJobSizeGauge.Set(float64(cache.GetSize()))
-		case NAMESPACE:
-			manager.namespaceSizeGauge.Set(float64(cache.GetSize()))
-		case PERSISTENTVOLUME:
-			manager.persistentVolumeSizeGauge.Set(float64(cache.GetSize()))
-		case PERSISTENTVOLUMECLAIM:
-			manager.pvcSizeGauge.Set(float64(cache.GetSize()))
-		case STORAGECLASS:
-			manager.storageClassSizeGauge.Set(float64(cache.GetSize()))
-		case INGRESS:
-			manager.ingressSizeGauge.Set(float64(cache.GetSize()))
-		case REPLICASET:
-			manager.replicasetSizeGauge.Set(float64(cache.GetSize()))
-		}
-
-	}
 
 	// set labels
 	manager.registerLock.RLock()
