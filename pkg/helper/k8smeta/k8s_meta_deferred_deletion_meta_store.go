2984f41cd4589e4e4dd715c3e5d3205c640f2b16
StartE <yili0618@qq.com>
2025-07-24 11:07:10 +0000

    fix comment

--- pkg/helper/k8smeta/k8s_meta_deferred_deletion_meta_store.go
+++ pkg/helper/k8smeta/k8s_meta_deferred_deletion_meta_store.go
@@ -225,7 +225,7 @@ func (m *DeferredDeletionMetaStore) handleAddOrUpdateEvent(event *K8sMetaEvent) 
 	key, err := m.keyFunc(event.Object.Raw)
 	if err != nil {
 		m.UpdateMetaStoreFailCounter()
-		logger.Error(context.Background(), "K8S_META_STORE_HANDLE_ALARM", "handle k8s meta with keyFunc error", err)
+		logger.Error(context.Background(), K8sMetaUnifyErrorCode, "handle k8s meta with keyFunc error", err)
 		return
 	}
 	idxKeys := m.getIdxKeys(event.Object)
