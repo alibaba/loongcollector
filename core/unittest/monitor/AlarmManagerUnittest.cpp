f70be042a5ef47f4988b0776fce6b151493d27cd
玄飏 <yangzehua.yzh@alibaba-inc.com>
2025-07-25 09:50:23 +0000

    fix ut

--- core/unittest/monitor/AlarmManagerUnittest.cpp
+++ core/unittest/monitor/AlarmManagerUnittest.cpp
@@ -53,7 +53,8 @@ void AlarmManagerUnittest::TestSendAlarm() {
         AlarmManager::AlarmVector& alarmBufferVec
             = *AlarmManager::GetInstance()->MakesureLogtailAlarmMapVecUnlocked(region);
 
-        std::string key = projectName + "_" + category + "_" + config;
+        std::string levelStr = "1"; // ALARM_LEVEL_WARNING
+        std::string key = projectName + "_" + category + "_" + config + "_" + levelStr;
         APSARA_TEST_EQUAL(1U, alarmBufferVec[alarmType].size());
         APSARA_TEST_EQUAL(true, alarmBufferVec[alarmType].find(key) != alarmBufferVec[alarmType].end());
         APSARA_TEST_EQUAL(category, alarmBufferVec[alarmType][key]->mCategory);
