8b7903719f0f00407e990cfa7f97347a188be5dd
wokron <yi1tang.yang@gmail.com>
2025-08-19 16:17:57 +0800

    add container discovery options

--- core/ebpf/Config.h
+++ core/ebpf/Config.h
@@ -23,6 +23,7 @@
 #include "json/json.h"
 
 #include "collection_pipeline/CollectionPipelineContext.h"
+#include "container_manager/ContainerDiscoveryOptions.h"
 #include "ebpf/include/export.h"
 
 namespace logtail::ebpf {
@@ -62,7 +63,10 @@ public:
               const CollectionPipelineContext* mContext,
               const std::string& sName);
     
+    bool mEnableSystemProfiling = false;
     std::vector<std::string> mCmdlines;
+    bool mEnableContainerDiscovery = false;
+    ContainerDiscoveryOptions mContainerDiscovery;
 };
 
 using PluginOptions = std::variant<SecurityOptions*, ObserverNetworkOption*, CpuProfilingOption*>;
