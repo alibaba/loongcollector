2d81fe3ec65e9c8422b1057a00e545e63b50639b
echochao <echochao@qq.com>
2025-08-22 00:21:22 +0000

    feat: Update Kafka configuration support, add producer and delivery configurations, optimize FlusherKafka class

--- core/dependencies.cmake
+++ core/dependencies.cmake
@@ -62,8 +62,11 @@ set(DEP_NAME_LIST
         yamlcpp
         zlib
         zstd
-        rdkafka
         )
+
+if (NOT ENABLE_ENTERPRISE)
+    list(APPEND DEP_NAME_LIST "rdkafka")
+endif()
 
 if (NOT NO_TCMALLOC)
     list(APPEND DEP_NAME_LIST "tcmalloc") # (gperftools)
--- core/unittest/flusher/FlusherKafkaUnittest.cpp
+++ core/unittest/flusher/FlusherKafkaUnittest.cpp
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#ifndef APSARA_UNIT_TEST_MAIN
 #define APSARA_UNIT_TEST_MAIN
+#endif
 
 #include <functional>
 #include <memory>
