12e628e57201befdec8a8d32b322b43496ce03c4
echochao <echochao@qq.com>
2025-10-22 20:10:21 +0800

    feat: Restore load.go

--- test/config/load.go
+++ test/config/load.go
@@ -15,7 +15,6 @@
 package config
 
 import (
-	"os"
 	"path/filepath"
 	"strings"
 )
@@ -49,18 +48,6 @@ func Load(path string) error {
 	EngineLogFile = reportDir + CaseName + "_engine.log"
 	LogDir = reportDir + CaseName + "_log"
 
-	if mkErr := os.MkdirAll(reportDir, 0o750); mkErr != nil {
-		return mkErr
-	}
-
-	FlusherFile = reportDir + CaseName + "_default_flusher.json"
-
-	if _, statErr := os.Stat(FlusherFile); os.IsNotExist(statErr) {
-		if writeErr := os.WriteFile(FlusherFile, []byte("{\"type\":\"flusher_stdout\",\"detail\":{}}\n"), 0o600); writeErr != nil {
-			return writeErr
-		}
-	} else if statErr != nil {
-		return statErr
-	}
+	FlusherFile = reportDir + CaseName + "default_flusher.json"
 	return nil
 }
