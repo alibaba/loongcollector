fa80ee9b460219e343f8d07b43ff7640447ae388
wokron <yi1tang.yang@gmail.com>
2026-03-05 14:40:48 +0800

    add missing format

--- core/ebpf/plugin/cpu_profiling/ProcessDiscoveryManager.h
+++ core/ebpf/plugin/cpu_profiling/ProcessDiscoveryManager.h
@@ -26,8 +26,8 @@
 #include "common/ProcParser.h"
 #include "common/StringTools.h"
 #include "container_manager/ContainerDiff.h"
-#include "logger/Logger.h"
 #include "ebpf/plugin/cpu_profiling/ProcessEntry.h"
+#include "logger/Logger.h"
 
 namespace logtail {
 namespace ebpf {
@@ -112,7 +112,9 @@ private:
         std::set<uint32_t> mPrevPids;
         std::map<uint32_t, bool> mPidMatchCache;
 
-        void FindAllMatch(const std::vector<ProcessEntry>& procsOrdered, std::vector<DiscoverEntry>& results, bool isContainerMode);
+        void FindAllMatch(const std::vector<ProcessEntry>& procsOrdered,
+                          std::vector<DiscoverEntry>& results,
+                          bool isContainerMode);
     };
 
     std::atomic_bool mRunning = false;
