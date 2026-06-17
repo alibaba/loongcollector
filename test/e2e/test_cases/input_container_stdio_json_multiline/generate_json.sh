252d160455d6f475705aa574cb2342ca06f34497
StartE <yili0618@qq.com>
2026-06-17 11:01:46 +0000

    add e2e

---
+++ test/e2e/test_cases/input_container_stdio_json_multiline/generate_json.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Output multi-line JSON blocks to stdout, each field on its own line.
+# Docker json-file driver will record each line separately;
+# MergeType=json should merge them back into complete JSON blocks.
+
+echo '{'
+echo '  "name": "test_multiline",'
+echo '  "value": 42,'
+echo '  "nested": {'
+echo '    "key": "hello"'
+echo '  }'
+echo '}'
+
+sleep 2
+
+echo '{"single_line": true}'
+
+sleep 90
